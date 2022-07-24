66f34fee97c2d31c714f5b39803f1a8e31509b48
hejin-jeong <pearlofthesea25@gmail.com>
2022-07-23 23:29:48 -0400

    Create navbar, footer, and first div of home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
+import HomePage from "./pages/HomePage"
+import AboutMePage from "./pages/AboutMePage"
+import ExperiencePage from "./pages/ExperiencePage"
+import ProjectPage from "./pages/ProjectPage"
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/aboutme' element={<AboutMePage />} />
+        <Route path='/experience' element={<ExperiencePage />} />
+        <Route path='/project' element={<ProjectPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
